153edc4ce1037a6f4bee2939832f413a1f11f0ce
Thomas Barks <tbarks@nmt.edu>
2018-03-07 19:01:12 -0700

    update so now objects load and swap with space bar

--- glitter/Sources/Object.h
+++ glitter/Sources/Object.h
@@ -1,13 +1,14 @@
-/******************************************************************************
-	Name: Amy Knowles
-	File Name: Object.h
-	Synopsis: This file contains variable and function declarations for the 
-			  Object class.
-******************************************************************************/
-
+//
+// Created by qidian on 3/2/18.
+//
 #pragma once
+#ifndef MODEL_VIEW_OBJECT_H
+#define MODEL_VIEW_OBJECT_H
+
+#endif //MODEL_VIEW_OBJECT_H
+
 #include <string>
-#include <GL/glew.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <fstream>
@@ -15,34 +16,34 @@
 #include <glm/vec3.hpp>	// glm::vec3
 #include <glm/vec4.hpp>	// glm::vec4
 #include <glm/mat4x4.hpp> // glm::mat4
-#include <glm/gtc/matrix_transform.hpp>	
+#include <glm/gtc/matrix_transform.hpp>
 // glm::translate, glm::rotate, glm::scale, glm::perspective
 
 class Object
 {
 public:
-	Object();
-	Object(std::string fileName);
-	void load(GLuint &program);
-	void draw();
-	void getObjInfo(std::string fileName);
-	~Object();
+    Object();
+    Object(std::string fileName);
+    void load(GLuint &program);
+    void draw();
+    void getObjInfo(std::string fileName);
+    ~Object();
 
 private:
-	int numVert;
-	int numNorm;
-	int numFaces;
-	int numIndex;
-	
-	std::vector<glm::vec4> vertList;
-	std::vector<glm::vec4> tempVertList;
-	std::vector<glm::vec4> normList;
-	std::vector<GLuint> vertexIndex;
-	std::vector<GLuint> normalsIndex;
-	glm::vec4 *vert, *color, *normal;
-	GLuint *index;
-	GLuint buffer, IndexBuffer;
+    unsigned long numVert;
+    unsigned long numNorm;
+    unsigned long numFaces;
+    unsigned long numIndex;
 
-	std::string title;
+    std::vector<glm::vec4> vertList;
+    std::vector<glm::vec4> tempVertList;
+    std::vector<glm::vec4> normList;
+    std::vector<GLuint> vertexIndex;
+    std::vector<GLuint> normalsIndex;
+    glm::vec4 *vert, *color, *normal;
+    GLuint *index;
+    GLuint buffer, IndexBuffer;
+
+    std::string title;
 };
 
--- glitter/Sources/main.cpp
+++ glitter/Sources/main.cpp
@@ -1,11 +1,13 @@
+#include <stdlib.h>
 #include "Object.h"
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 //settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+const unsigned int numObjects = 2;
+static unsigned int curObject = 0;
 
 const char *vertexShaderSource = "#version 150\n"
         "in vec4 vPosition;\n"
@@ -25,10 +27,8 @@ const char *fragmentShaderSource = "#version 150\n"
         "   FragColor = fColor;\n"
         "}\n\0";
 
-int main(){
-
-    // glfw: initialize and configure
-    // ------------------------------
+int init(GLFWwindow **windowLoc, GLuint *shaderProgram)
+{
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -38,26 +38,29 @@ int main(){
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Anderson_Barks Triangle", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Anderson_Barks Model Loading and Swapping", nullptr, nullptr);
     if(!window){
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
+    windowLoc[0] = window;
     glfwMakeContextCurrent(window);
-    //glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetKeyCallback(window, key_callback);
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
         std::cout << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
         return -1;
     }
 
     // build and compile our shader program
     // ------------------------------------
     // vertex shader
-    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    int successFlag = 0;
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
     glCompileShader(vertexShader);
     // check for shader compile errors
@@ -68,9 +71,10 @@ int main(){
     {
         glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
         std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        successFlag = -1;
     }
     // fragment shader
-    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
     glCompileShader(fragmentShader);
     // check for shader compile errors
@@ -79,74 +83,72 @@ int main(){
     {
         glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
         std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        successFlag = -1;
     }
     // link shaders
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
+    *shaderProgram = glCreateProgram();
+    glAttachShader(*shaderProgram, vertexShader);
+    glAttachShader(*shaderProgram, fragmentShader);
+    glLinkProgram(*shaderProgram);
     // check for linking errors
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    glGetProgramiv(*shaderProgram, GL_LINK_STATUS, &success);
     if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        glGetProgramInfoLog(*shaderProgram, 512, nullptr, infoLog);
         std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        successFlag = -1;
     }
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    glUseProgram(shaderProgram);
+    glUseProgram(*shaderProgram);
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glEnable(GL_DEPTH_TEST);
 
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
-    /*float vertices[] = {
-            -0.5f, -0.5f, 0.0f, // left
-            0.5f, -0.5f, 0.0f, // right
-            0.0f,  0.5f, 0.0f  // top
-    };*/
-    Object *o1 = new Object("assets/Pikachu.obj");
+    return successFlag;
+}
 
-    GLuint VAO;
-    glGenVertexArrays(1, &VAO);
-    //glGenBuffers(1, &VBO);
+void initObjects(unsigned int numObjects, Object **obj, GLuint *vao, GLuint shaderProgram)
+{
+    obj[0] = new Object("assets/cube.obj");
+    obj[1] = new Object("assets/Pikachu.obj");
+
+    glGenVertexArrays(numObjects, vao);
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
-    o1->load(shaderProgram);
+    for (unsigned int i = 0; i < numObjects; i++) {
+        glBindVertexArray(vao[i]);
+        obj[i]->load(shaderProgram);
+    }
+}
 
-    /*glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(o1), o1, GL_STATIC_DRAW);
+int main(){
 
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+    // glfw: initialize and configure
+    // ------------------------------
+    GLFWwindow **windowLoc = (GLFWwindow **) malloc(sizeof(GLFWwindow *));
+    GLuint *shaderProgram = (GLuint *) malloc(sizeof(GLuint));
+    int successFlag = init(windowLoc, shaderProgram);
+    if (successFlag < 0) {
+        free(windowLoc);
+        free(shaderProgram);
+        glfwTerminate();
+        return -1;
+    }
+    GLFWwindow *window = windowLoc[0];
+    free(windowLoc);
 
-    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0);*/
-
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    //glBindVertexArray(0);
-
-    // uncomment this call to draw in wireframe polygons.
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    Object **obj = (Object **) malloc(sizeof(Object **) * numObjects);
+    GLuint *vao = (GLuint *) malloc(sizeof(GLuint *) * numObjects);
+    initObjects(numObjects, obj, vao, *shaderProgram);
 
     // render loop
     // -----------
     while(!glfwWindowShouldClose(window)){
-
-        // input
-        // -----
-        processInput(window);
-
         // render
         // ------
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // draw our first triangle
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        o1->draw();
-        //glDrawArrays(GL_TRIANGLES, 0, 3);
-        //glBindVertexArray(0); // no need to unbind it every time
+        glBindVertexArray(vao[curObject]); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        obj[curObject]->draw();
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -156,9 +158,13 @@ int main(){
 
     // optional: de-allocate all resources once they've outlived their purpose:
     // ------------------------------------------------------------------------
-    delete o1;
-    glDeleteVertexArrays(1, &VAO);
-    //glDeleteBuffers(1, &VBO);
+    free(shaderProgram);
+    for (unsigned int i = 0; i < numObjects; i++) {
+        delete obj[i];
+    }
+    free(obj);
+    glDeleteVertexArrays(numObjects, vao);
+    free(vao);
 
     // glfw: terminate, clearing all previously allocated GLFW resources
     // -----------------------------------------------------------------
@@ -166,18 +172,11 @@ int main(){
     return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow* window){
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
-        glfwSetWindowShouldClose(window, true);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        exit(EXIT_SUCCESS);
+    if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) {
+        curObject = (curObject + 1) % numObjects;
     }
-}
-
-//glfw: whenever the window size changed (by OS or user resize) this callback function executes
-void framebuffer_size_callback(GLFWwindow* window, int width, int height){
-
-    // make sure the viewport matches the new window dimensions; note that width and
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0,0,width, height);
 }
